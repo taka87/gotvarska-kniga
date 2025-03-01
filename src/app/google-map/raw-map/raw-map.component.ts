abc0b7bc8adcaba567a3403ca2fd9b9deca848ee
Dani <daredevil@dir.bg>
2025-03-01 19:31:12 +0200

    Some small settings implemented ...

--- src/app/google-map/raw-map/raw-map.component.ts
+++ src/app/google-map/raw-map/raw-map.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-raw-map',
@@ -7,30 +8,41 @@ import { GoogleMapsModule } from '@angular/google-maps';
   templateUrl: './raw-map.component.html',
   styleUrl: './raw-map.component.css'
 })
-export class RawMapComponent {
+export class RawMapComponent implements OnInit {
   private map!: google.maps.Map; // Запазваме картата в свойство
-  
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.loadMap();
-    }, 500);
 
-    // window.addEventListener('resize', () => {
-    //   if (this.map) {
-    //     google.maps.event.trigger(this.map, 'resize');
-    //     this.map.setCenter({ lat: 42.50666, lng: 27.484537 }); // Бургас
-    //   }
-    // });
+  ngOnInit(): void {
+    //console.log('Google Maps API Key:', environment.googleMapsApiKey); // Провери дали се зарежда правилно
+    this.loadGoogleMaps(); // Зареждаме API-то
+  }
+
+  loadGoogleMaps(): void {
+    if (window['google'] && window['google'].maps) {
+      console.log('Google Maps API вече е зареден.');
+      this.loadMap(); // Ако API-то е заредено, директно зареждаме картата
+      return;
+    }
+
+    const script = document.createElement('script');
+    script.id = 'google-maps';
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${environment.googleMapsApiKey}&libraries=places`;
+    script.async = true;
+    script.defer = true;
+    script.onload = () => {
+      console.log('Google Maps API зареден успешно!');
+      this.loadMap(); // Когато API-то се зареди, създаваме картата
+    };
+
+    document.body.appendChild(script);
   }
 
   loadMap(): void {
     const mapElement = document.getElementById('map');
+    
     if (!mapElement) {
       console.error("Картата не е намерена в DOM-а.");
       return;
     }
-    // console.log("Картата е намерена, зареждам...");
-    // this.loadMap();
 
     this.map = new google.maps.Map(mapElement as HTMLElement, {
       center: { lat: 42.499851, lng: 27.480515 },
@@ -40,10 +52,7 @@ export class RawMapComponent {
       styles: [{ featureType: "poi", elementType: "labels", stylers: [{ visibility: "off" }] }]
     });
 
-    
-
     new google.maps.Marker({
-      // position: { lat: 42.5048, lng: 27.4626 },
       position: { lat: 42.499851, lng: 27.480515 },
       map: this.map,
       icon: {
