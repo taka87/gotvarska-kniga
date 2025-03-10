1b9b3b7b6173666ed4606b7c277cdbaa9db28db8
Dani <daredevil@dir.bg>
2025-03-10 19:35:11 +0200

    Add environment.prod.ts for production build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
-    production: true,
-    googleMapsApiKey: ''
-  };   
+  production: true,
+  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
+  apiUrl: 'YOUR_PRODUCTION_API_URL',
+  supabaseUrl: 'YOUR_SUPABASE_URL',
+  supabaseKey: 'YOUR_SUPABASE_KEY'
+};
