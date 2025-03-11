6746a7be1fc8d7a1eeb40fb20a7aea70440f2e01
Dani <daredevil@dir.bg>
2025-03-11 23:46:51 +0200

    Fix Variable names

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'googleMapsApiKey',
-  NEXT_PUBLIC_SUPABASE_URL: 'supabaseUrl',
-  NEXT_PUBLIC_SUPABASE_ANON_KEY: 'supabaseKey'
+  supabaseUrl: 'NEXT_PUBLIC_SUPABASE_URL',
+  supabaseKey: 'NEXT_PUBLIC_SUPABASE_ANON_KEY'
 };
