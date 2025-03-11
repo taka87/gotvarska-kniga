83449c9e4afe01f7a993d6c22685c2a9fd961657
Dani <daredevil@dir.bg>
2025-03-11 22:36:15 +0200

    Fix supabaseClient.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register } from 'swiper/element/bundle';
+import { environment } from './environments/environment';
 
 // Регистрирай Swiper като custom element
 register();
@@ -9,4 +10,5 @@ register();
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
+  console.log('ENV:', environment);
   // new for MYSQL??
