c6879b9c5947c061ecd271b880709b1f48de15fa
Dani <daredevil@dir.bg>
2025-03-12 16:24:28 +0200

    some changes test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,3 +4,8 @@ export const environment = {
   NEXT_PUBLIC_SUPABASE_URL: 'NEXT_PUBLIC_SUPABASE_URL',
   NEXT_PUBLIC_SUPABASE_ANON_KEY: 'NEXT_PUBLIC_SUPABASE_ANON_KEY'
 };
+
+console.log("✅ Production environment loaded!");
+console.log("🔍 Supabase URL:->", environment.googleMapsApiKey);
+console.log("🔍 Supabase Key:->>", environment.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+console.log("🔍 Supabase Key: ->>>", environment.NEXT_PUBLIC_SUPABASE_URL);
