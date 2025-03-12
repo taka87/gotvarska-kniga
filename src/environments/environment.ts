304c53d4fcf2f567612e8be5718a9686a5daf4a0
Dani <daredevil@dir.bg>
2025-03-12 02:43:07 +0200

    Add environment.ts with initial configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   googleMapsApiKey: '',
-  NEXT_PUBLIC_SUPABASE_URL: 'https://.supabase.co',
+  NEXT_PUBLIC_SUPABASE_URL: '',
   NEXT_PUBLIC_SUPABASE_ANON_KEY: ''
 };
