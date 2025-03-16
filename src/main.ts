05828ad1b0aaf3bab8dc5f00f7caf6013064fb40
Dani <daredevil@dir.bg>
2025-03-16 23:44:30 +0200

    try to back 2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { register } from 'swiper/element/bundle';
@@ -7,12 +6,6 @@ import { register } from 'swiper/element/bundle';
 // Регистрирай Swiper като custom element
 register();
 
-// bootstrapApplication(AppComponent, {
-//   providers: [
-//     provideHttpClient(),
-//     ...appConfig.providers
-//   ]
-// }).catch((err) => console.error(err));
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
