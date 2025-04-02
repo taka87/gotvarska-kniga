eb639bd3d284235e36f5cfa3b224f29d813f844f
Dani <daredevil@dir.bg>
2025-04-02 22:11:04 +0300

    try environment

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+import { environmentVariables } from "./environment.variables";
+
+export const environment = {
+  production: false,
+  googleMapsApiKey: environmentVariables.googleMapsApiKey ,
+  NEXT_PUBLIC_SUPABASE_URL: environmentVariables.NEXT_PUBLIC_SUPABASE_URL,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY: environmentVariables.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  apiFoodSearchUrl: environmentVariables.apiFoodSearchUrl
+};
